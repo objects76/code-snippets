e3fbe8bc8bffec3020eff5b36589118637eb396b
jjkim <objects76@gmail.com>
2020-11-25 22:08:31 +0900

    + clearall

--- clearall.js
+++ clearall.js
@@ -1,40 +1,31 @@
 //
 // ref: https://github.com/skt-t1-byungi/clearall/blob/master/index.ts
 //
+
+//
+// with closure, what if JS support dtor????
+//
 export default function add(target, type, listener, ...args) {
-  class Cleaner {
-    constructor() {
-      this.actions = [];
-    }
-    destroy = () => this.actions.forEach((v) => v());
-    add = (target, type, listener, ...args) => {
+  const actions = [];
+  const closure = () => actions.splice(0).forEach((v) => v());
+  closure.add = (target, type, listener, ...args) => {
+    if (target && listener) {
       console.debug(`subscribe for ${type}`);
       const on = target.addEventListener || target.addListener || target.on || target.subscribe;
       if (typeof on !== "function") throw new TypeError("Target is not listenable.");
 
       const f = on.call(target, type, listener, ...args);
       if (typeof f === "function") {
-        this.actions.push(f);
+        actions.push(f);
       } else {
         const off = target.removeEventListener || target.removeListener || target.off || target.unsubscribe;
-        if (typeof off === "function") this.actions.push(() => off.call(target, type, listener));
+        if (typeof off === "function") actions.push(() => off.call(target, type, listener));
       }
+    }
+    return closure;
+  };
 
-      return clearClosure;
-    };
-  }
-
-  let theCleaner = new Cleaner();
-  if (target && listener) theCleaner.add(target, type, listener, ...args); // first add.
-
-  function clearClosure() {
-    theCleaner.destroy();
-    theCleaner = undefined;
-    delete clearClosure.add;
-  }
-  clearClosure.add = theCleaner.add; // following add.
-
-  return clearClosure;
+  return closure.add(target, type, listener, ...args);
 }
 
 //
@@ -45,5 +36,7 @@ if (window.ClearAll) {
     .add(window, "beforeunload", () => console.log("beforeunload"))
     .add(window, "unload", () => console.log("unload"));
 
-  addTestWidget("<button>clear</button>", () => closeAll());
+  addTestWidget("<button>clear</button>", () => {
+    closeAll();
+  });
 }
